c0b28ad3b8568494b7346c98b1dc0eb0a29c36c7
mahesh-c-pathak <93431683+mahesh-c-pathak@users.noreply.github.com>
2024-02-07 08:13:59 +0000

    youtube.py first commit

---
+++ youtube-watcher.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import logging
+import sys
+def main():
+    logging.info("START")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    sys.exit(main())
