f9a0cf45534fe1856f9b0dd3b324e73df05ca04f
mahesh-c-pathak <93431683+mahesh-c-pathak@users.noreply.github.com>
2024-02-12 03:16:52 +0000

    added producer.py

---
+++ producer.py
@@ -0,0 +1,19 @@
+from kafka import KafkaProducer
+import json
+from data import get_registerd_user
+import time
+
+def json_serrializer(data):
+    return json.dumps(data).encode("utf-8")
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serrializer=json_serrializer
+)
+
+if __name__ == "__main__":
+    while 1==1:
+        registered_user = get_registerd_user()
+        print(registered_user)
+        producer.send("my_topic", registered_user)
+        time.sleep(4)
